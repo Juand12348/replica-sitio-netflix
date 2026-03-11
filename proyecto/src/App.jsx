6fa724c0ac1d57f5db518df99330cbd0f32c229e
Matias-RamirezT <matias.ramirezt@uniagustiniana.edu.co>
2026-03-11 16:54:25 +0000

    Agrega componente FAQ estilo Netflix

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import DavidMartinez from './DavidMartinez'
+import MatiasRamirez from './MatiasRamirez'
 
 function App() {
   return (
     <>
       <DavidMartinez />
+      <MatiasRamirez />
     </>
   )
 }
