44877f8a745f8436d389508c19fb4c7ce13a6d44
duvan-carvajal <duvan.carvajalp@uniagustiniana.edu.co>
2026-03-11 23:03:35 +0000

    Seccion Principal + Imagen

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Principal from './Principal'
 import DavidMartinez from './DavidMartinez'
 
 function App() {
   return (
     <>
+    <header className = "cabecera">
+      {/* en esta linea va la navbar, debe tener position:absolute asi la imagen no se mueve hacia abajo */}
+      <Principal />
+      </header>
       <DavidMartinez />
     </>
   )
